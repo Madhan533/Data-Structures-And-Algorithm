9106116047a2b949e9540f49682e58e21ca42ad3
PM Madhan533 <madhan.pm2006@gmail.com>
2025-12-30 09:19:56 +0530

    Update Stack using Array.c

--- Stack/Stack using Array.c
+++ Stack/Stack using Array.c
@@ -1,29 +1,36 @@
-//Stack using Array
+//Stack implementation using Array
 
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>
+
 #define MAX 5
+
 int stack[MAX];
 int top=-1;
+
 int main()
 {
     int n,val;
-    printf("Enter no.of values (0 to %d): ",MAX);
+    printf("Enter no. of values to insert (0 to %d) : ",MAX);
     scanf("%d",&n);
+    
     if(n>MAX)
     {
-        printf("stack overflow\n");
+        printf("Stack Overflow\n");
         return 0;
     }
+    
     for(int i=1;i<=n;i++)
     {
-        printf("enter value %d : ",i);
+        printf("Enter value %d : ",i);
         scanf("%d",&val);
         stack[++top]=val;
     }
-     printf("values in stack : ");
-     for(int i=top;i>=0;i--)
-     {
-         printf("%d ",stack[i]);
-     }
+    
+    printf("Values in Stack : ");
+    for(int i=top;i>=0;i--)
+    {
+        printf("%d ",stack[i]);
+    }
+    
 }
